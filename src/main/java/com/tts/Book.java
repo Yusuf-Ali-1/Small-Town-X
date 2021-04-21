d6f7ea9463f158ba327e4424d13045f199ac8fe1
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-04-21 13:42:21 -0400

    1st commit

--- src/main/java/com/tts/Book.java
+++ src/main/java/com/tts/Book.java
@@ -1,45 +1,50 @@
 package com.tts;
 
 public class Book {
-
     String title;
     boolean borrowed;
 
-    // Creates a new Book
+    public Book() {
+    }
+
+    public Book(String title, boolean borrowed) {
+        this.title = title;
+        this.borrowed = borrowed;
+    }
+
+    public String toString() {
+        return "Book{title='" + this.title + "', borrowed=" + this.borrowed + "}";
+    }
+
     public Book(String bookTitle) {
-        // Implement this method
-        title = bookTitle;
-        borrowed = false;
+        this.title = bookTitle;
+        this.borrowed = false;
     }
 
-    public void rented(){
-        borrowed = true;
-    }
-
-    // Marks the book as rented
     public void borrowed() {
-        // Implement this method
+        this.borrowed = true;
     }
 
-    // Marks the book as not rented
     public void returned() {
-        // Implement this method
-        borrowed = false;
+        this.borrowed = false;
     }
 
-    // Returns true if the book is rented, false otherwise
     public boolean isBorrowed() {
-        // Implement this method
         return this.borrowed;
-
     }
 
-    // Returns the title of the book
     public String getTitle() {
-        // Implement this method
-
-        return title;
+        return this.title;
     }
 
+    public static void main(String[] arguments) {
+        Book example = new Book("The Da Vinci Code");
+        System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        example.borrowed();
+        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+        example.returned();
+        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+    }
 
 }
--- src/main/java/com/tts/Library.java
+++ src/main/java/com/tts/Library.java
@@ -1,51 +1,87 @@
-//package com.tts;
 //
-//public class Library {
-//    // Add the missing implementation to this class
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
 //
-//    public static void main(String[] args) {
-//        // Create two libraries
-//        Library firstLibrary = new Library("10 Main St.");
-//        Library secondLibrary = new Library("228 Liberty St.");
-//
-//        // Add four books to the first library
-//        firstLibrary.addBook(new Book("The Da Vinci Code"));
-//        firstLibrary.addBook(new Book("Le Petit Prince"));
-//        firstLibrary.addBook(new Book("A Tale of Two Cities"));
-//        firstLibrary.addBook(new Book("The Lord of the Rings"));
-//
-//        // Print opening hours and the addresses
-//        System.out.println("Library hours:");
-//        printOpeningHours();
-//        System.out.println();
-//
-//        System.out.println("Library addresses:");
-//        firstLibrary.printAddress();
-//        secondLibrary.printAddress();
-//        System.out.println();
-//
-//        // Try to borrow The Lords of the Rings from both libraries
-//        System.out.println("Borrowing The Lord of the Rings:");
-//        firstLibrary.borrowBook("The Lord of the Rings");
-//        firstLibrary.borrowBook("The Lord of the Rings");
-//        secondLibrary.borrowBook("The Lord of the Rings");
-//        System.out.println();
-//
-//        // Print the titles of all available books from both libraries
-//        System.out.println("Books available in the first library:");
-//        firstLibrary.printAvailableBooks();
-//        System.out.println();
-//        System.out.println("Books available in the second library:");
-//        secondLibrary.printAvailableBooks();
-//        System.out.println();
-//
-//        // Return The Lords of the Rings to the first library
-//        System.out.println("Returning The Lord of the Rings:");
-//        firstLibrary.returnBook("The Lord of the Rings");
-//        System.out.println();
-//
-//        // Print the titles of available from the first library
-//        System.out.println("Books available in the first library:");
-//        firstLibrary.printAvailableBooks();
-//    }
-//}
+
+package com.tts;
+
+import java.util.ArrayList;
+
+public class Library {
+    String libraryAddress;
+    ArrayList<Book> books;
+
+    public Library(String address) {
+        this.libraryAddress = address;
+        this.books = new ArrayList();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public void printAddress() {
+        System.out.println(this.libraryAddress);
+    }
+
+    public void printAvailableBooks() {
+        boolean libraryIsEmpty = true;
+
+        for(int i = 0; i < this.books.size(); ++i) {
+            Book libraryBook = (Book)this.books.get(i);
+            if (!libraryBook.isBorrowed()) {
+                System.out.println(libraryBook.getTitle());
+                libraryIsEmpty = false;
+            }
+        }
+
+        if (libraryIsEmpty) {
+            System.out.println("Sorry, our catalog is empty");
+        }
+
+    }
+
+    public String borrowBook(String bookTitle) {
+        for(int i = 0; i < this.books.size(); ++i) {
+            Book libraryBook = (Book)this.books.get(i);
+            String libraryBookTitle = libraryBook.getTitle();
+            if (libraryBookTitle.equals(bookTitle)) {
+                if (!libraryBook.isBorrowed()) {
+                    libraryBook.borrowed();
+                    System.out.println("We have " + libraryBookTitle + ", and you have successfully borrowed it!");
+                    return null;
+                }
+
+                System.out.println("Someone else has " + libraryBookTitle + " right now :(");
+                return null;
+            }
+        }
+
+        System.out.println("Your book was not found in our system, sorry.");
+        return null;
+    }
+
+    public String returnBook(String bookTitle) {
+        boolean found = false;
+
+        for(int i = 0; i < this.books.size(); ++i) {
+            Book libraryBook = (Book)this.books.get(i);
+            String libraryBookTitle = libraryBook.getTitle();
+            if (libraryBookTitle.equals(bookTitle)) {
+                if (libraryBook.isBorrowed()) {
+                    libraryBook.returned();
+                    System.out.println("You successfully returned " + libraryBookTitle + "!");
+                    found = true;
+                    break;
+                }
+
+                if (!found) {
+                    System.out.println("Are you sure you got the book from here? It isn't in our system.");
+                }
+            }
+        }
+
+        System.out.println();
+        return null;
+    }
+}
